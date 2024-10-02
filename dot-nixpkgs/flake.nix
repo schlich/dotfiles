2b7b2a67dded318665b0fd8b3a2da7b55bcc111b
Ty <tschlic@collagegroup.com>
2024-10-01 20:19:03 -0500

    update flake

--- dot-nixpkgs/flake.nix
+++ dot-nixpkgs/flake.nix
@@ -5,10 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # home-manager.url = "github:nix-community/home-manager";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-    outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: 
+    outputs = inputs@{ nixpkgs, darwin, ... }: 
     { 
       darwinConfigurations."Ty-Schlichenmeyer-MacBook-Air" = darwin.lib.darwinSystem {
         modules = [
